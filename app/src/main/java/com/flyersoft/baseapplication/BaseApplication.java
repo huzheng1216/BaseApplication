0640067910c9df69e3d3543d11b7dfc6e3458f7d
胡正 <zheng.hu@inveno.com>
2019-09-20 15:36:40 +0800

    first commit

---
+++ app/src/main/java/com/flyersoft/baseapplication/BaseApplication.java
@@ -0,0 +1,21 @@
+package com.flyersoft.baseapplication;
+
+import android.app.Application;
+
+/**
+ * creat by: huzheng
+ * date: 2019-09-20
+ * description:
+ * （分模块开发）基础application
+ */
+public abstract class BaseApplication extends Application {
+
+    //各个模块的初始化方法
+    public abstract void init();
+
+    //修改顶部栏颜色
+    public abstract void setStatusBarColor(int color);
+
+    //搜索栏是否显示
+    public abstract void setSearchBarVisible(boolean visible);
+}
